c230c1a4e60ff00d275d0b8dd352f4168217056f
jdn0215 <jdn0213@gmail.com>
2017-03-30 07:31:02 -0600

    consulta_de_registros

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ router.route('/load').post((req,res)=>{
 				   res.json({mj:errConectClientM});
 		}
 		///obteniendo ubicación del usuario
+		console.log("objeto recibido: "+obj);
 		let obj = req.body;
 		let latitud = JSON.parse(obj.arg0);
 		let longitud = JSON.parse(obj.arg1);
