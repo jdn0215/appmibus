7c864714be689bbc61323bfe062ff25ab6129c3d
jdn0215 <jdn0213@gmail.com>
2017-04-07 08:22:19 -0600

    tratando de arreglar la torta.3

--- public/js/initMap.js
+++ public/js/initMap.js
@@ -125,6 +125,7 @@ const eventoInfoWindows=()=>{
 		e.setAttribute("id","id"+key);
 		e.setAttribute("data-toggle","popover");
 		$("#id"+key).click(()=>{
+			alert("caca");
 			let m = marcasObjs[key];
 			$("#dataUsuario")[0].innerHTML= ("  "+m.usuario);
 			$("#dataOrigen")[0].innerHTML=("  "+m.origen);
