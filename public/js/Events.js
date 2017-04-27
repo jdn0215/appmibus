788995d4d371ddce2116929f6747ba9d02701435
marsaoth <javier.zamora.calvo@est.una.ac.cr>
2017-04-26 23:46:38 -0500

    ya mejor2

--- public/js/Events.js
+++ public/js/Events.js
@@ -67,7 +67,15 @@ const addPopOver=()=>{
 
 
 const popOverBuscar=()=>{
-	fMensaje("TODO");
+	// $("#"+botonBuscar)[0].setAttribute("data-content",forms.formSea);
+	// $("#"+botonBuscar)[0].setAttribute("data-toggle","popover");
+	// $("#"+botonBuscar)[0].setAttribute("title","<strong>Buscar un registro</strong>");
+	// $("#"+botonBuscar)[0].setAttribute("data-placement","top");
+	// $("#"+botonBuscar)[0].setAttribute("data-trigger","click");
+	// $("#"+botonBuscar).popover({
+		// callback:()=>{initEventsSea();},
+		// html:true
+	// });
 }
 
 const initEventsAdd=()=>{
@@ -78,6 +86,13 @@ const initEventsAdd=()=>{
 	$("#addQuePasa").focus(()=>window.scrollTo(0, 0));
 	$("#addExtra").focus(()=>window.scrollTo(0, 0));
 };
+
+// const initEventsSea=()=>{
+	// $("#seaSea").click(()=>seaReporte()); 
+	// $("#seaCancel").click(()=> $("#"+botonBuscar)[0].click());
+	// $("#seaOrigen").focus(()=>window.scrollTo(0, 0));
+	// $("#seaDestion").focus(()=>window.scrollTo(0, 0));
+// };
 
 const addReporte=()=>{
 	let reporte = createReporte();
@@ -96,6 +111,23 @@ const addReporte=()=>{
 	$("#addCancel")[0].click();
 };
 
+/* const seaReporte=()=>{
+	let reporte = createSeaReporte();
+	if(reporte === false ) return;
+	proxy.proxy('save',res=>{
+		let r = res.mj;
+		fMensaje(     r===success          ? mj_Add_succes
+				:     r===errConectClientM ? mj_Add_ERR_DB
+				:     r===errTrans         ? mj_Add_ERR_SR
+				:                            mj_Add_ERR_UNK
+				);
+	},reporte);
+	marcasObjs.push(reporte);
+	aux = true;
+	aux2 = true;
+	$("#seaCancel")[0].click();
+}; */
+
 
 ///////////////////////***creacíon del reporte****************////////////////////
 const createReporte=()=>
@@ -111,6 +143,17 @@ const createReporte=()=>
 		localStorage.getItem(USER_NAME)
 	):false;
 	
+/* const createSeaReporte=()=>
+	validarSea()?
+	new Marca(
+		current.position.lat(),
+		current.position.lng(),
+		$("#seaOrigen").val(),
+		$("#SeaDestion").val(),
+		null,
+		localStorage.getItem(USER_NAME)
+	):false; */
+	
 const validar=()=>{
 	let inputs=["addOrigen","addDestion","addQuePasa"];
 	let result = true;
@@ -122,6 +165,18 @@ const validar=()=>{
 	});
 	return result;
 }	
+
+/* const validarSea=()=>{
+	let inputs=["seaOrigen","seaDestion"];
+	let result = true;
+	inputs.forEach(e=>{
+		if($("#"+e).val()===""){
+			result = false;
+			changeColorBorder(e);
+		}else changeColorBorder(e,true);
+	});
+	return result;
+} */	
 
 	
 const changeColorBorder=(id,state=false)=> $("#"+id)[0].style=state?"":"border:medium double red;";
