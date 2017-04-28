59054a15a853e0a27f1e692b18284dfbe4a8444f
jdn0215 <jdn0213@gmail.com>
2017-04-27 21:18:24 -0600

    detallito en ubicame

--- public/js/values.js
+++ public/js/values.js
@@ -38,7 +38,7 @@ const responseStatusMin=200,
 	
 const forms={	
 	formAdd:`
-	<div id="addReporte" class="popOverForm">
+	<div id="addReporte" class="popOverForm Fscroll">
 		<label for="addOrigen">
 			Origen del bus
 			<input id="addOrigen"></input>
